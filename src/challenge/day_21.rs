e254198f9ebcde634443167f5950a3b0021d3b43
Metod <metod@medja.net>
2023-12-21 21:12:09 +0100

    optimize(21): Optimize step simulation by splitting it into quadrants

--- src/challenge/day_21.rs
+++ src/challenge/day_21.rs
@@ -1,7 +1,7 @@
 use rustc_hash::FxHashSet;
 
 pub fn part_a(input: &[&str]) -> anyhow::Result<impl std::fmt::Display> {
-    Ok(simulate(input, 64).len())
+    Ok(simulate(input, 64, Quadrant::All).len())
 }
 
 // Once we've reached the edge of the 2rd grid (counting from 0) a pattern starts to emerge:
@@ -43,12 +43,22 @@ pub fn part_b(input: &[&str]) -> anyhow::Result<impl std::fmt::Display> {
 
     // assume the grid is square
     let size = input.len() as isize;
+    let center = size / 2;
 
     // size / 2 gets us to the edge of the first grid, each additinal size steps gets us
     // to the edge of the next grid.
     // This is just enough to observe all of the unique grid arrangements.
-    let min_steps = 2 * size + size / 2;
-    let positions = simulate(input, min_steps as usize);
+    let min_steps = (2 * size + size / 2) as usize;
+
+    let top_left = simulate(input, min_steps, Quadrant::TopLeft).into_iter();
+    let top_right = simulate(input, min_steps, Quadrant::TopRight).into_iter();
+    let bottom_left = simulate(input, min_steps, Quadrant::BottomLeft).into_iter();
+    let bottom_right = simulate(input, min_steps, Quadrant::BottomRight).into_iter();
+
+    let positions = top_left
+        .chain(top_right.filter(|(x, _)| *x != center))
+        .chain(bottom_left.filter(|(_, y)| *y != center))
+        .chain(bottom_right.filter(|(x, y)| *x != center && *y != center));
 
     let mut center_a = 0u64; // A grids
     let mut center_b = 0u64; // B grids
@@ -77,10 +87,11 @@ pub fn part_b(input: &[&str]) -> anyhow::Result<impl std::fmt::Display> {
         + corners)
 }
 
-fn simulate(input: &[&str], steps: usize) -> FxHashSet<(isize, isize)> {
+fn simulate(input: &[&str], steps: usize, quadrant: Quadrant) -> FxHashSet<(isize, isize)> {
     // assume the grid is square
     let size = input.len() as isize;
-    let start = find_start(input);
+    // assume the starting position is at the center
+    let start = (size / 2, size / 2);
 
     let mut current_steps = FxHashSet::default();
     let mut next_steps = FxHashSet::default();
@@ -91,6 +102,14 @@ fn simulate(input: &[&str], steps: usize) -> FxHashSet<(isize, isize)> {
             for (dx, dy) in [(1, 0), (0, 1), (-1, 0), (0, -1)] {
                 let x = x + dx;
                 let y = y + dy;
+
+                match quadrant {
+                    Quadrant::TopLeft if x > start.0 || y > start.1 => continue,
+                    Quadrant::TopRight if x < start.0 || y > start.1 => continue,
+                    Quadrant::BottomLeft if x > start.0 || y < start.1 => continue,
+                    Quadrant::BottomRight if x < start.0 || y < start.1 => continue,
+                    _ => {}
+                };
 
                 let gx = x.rem_euclid(size) as usize;
                 let gy = y.rem_euclid(size) as usize;
@@ -109,22 +128,18 @@ fn simulate(input: &[&str], steps: usize) -> FxHashSet<(isize, isize)> {
     current_steps
 }
 
-fn find_start(input: &[&str]) -> (isize, isize) {
-    input
-        .iter()
-        .enumerate()
-        .find_map(|(y, row)| {
-            row.bytes()
-                .position(|char| char == b'S')
-                .map(|x| (x as isize, y as isize))
-        })
-        .unwrap()
-}
-
 fn grid_index(position: isize, size: isize) -> isize {
     if position < 0 {
         (position + 1) / size - 1
     } else {
         position / size
     }
+}
+
+enum Quadrant {
+    All,
+    TopLeft,
+    TopRight,
+    BottomLeft,
+    BottomRight,
 }
